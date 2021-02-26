cd1cea4620e6e27ab53c2aa39a28e80b81c1fcf2
adrlvma <adrlvma741@gmail.com>
2021-02-26 17:56:55 -0300

    Implementando método de envio e rotina de chegada constante de mensagem.

--- cliente.py
+++ cliente.py
@@ -11,19 +11,47 @@ Fontes:
 
 '''
 
+from datetime import datetime
 from threading import Thread
 import rpyc
 
-class Cliente():
+last_msg = 0
 
-    def _run(self):
-        print("Olá, seja bem vindo ao chat Educado.")
-        
-        server = rpyc.connect("localhost", 18861)
-        nome = input()
+def receber_mensagem():
+    global last_msg
 
-        print(server.root.exposed_informe_nome(nome))
+    ultimo = 0.0
+    while True:
+        try:
+            s_atual = datetime.now().second
+            if abs(s_atual-ultimo) > 0.5: 
+                msg = server.root.exposed_return_mensage(last_msg)
+                ultimo = s_atual
 
-cl = Cliente()
+                last_msg += len(msg)
 
-cl._run()
+                if len(msg)>0:
+                    for m in msg:
+                        print(m)
+        except OSError:
+            break
+
+
+print("Olá, seja bem vindo ao chat Educado.")
+
+server = rpyc.connect("localhost", 18861)
+nome = input("Diga seu nome:")
+
+nome_resp = server.root.exposed_informe_nome(nome)
+
+print(nome_resp)
+
+receive_thread = Thread(target=receber_mensagem)
+receive_thread.start()
+
+last_msg = 0
+
+while True:
+    msg = input()
+    server.root.exposed_enviar_mensagem(nome, msg)
+    last_msg+=1
